5080789fc6f1501dbaecb609a2ba6f3ab57dda29
Shripad <shripad1020@gmail.com>
2021-04-27 23:20:59 +0530

    Predictions on local machine complete

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import tensorflow as tf
-from utils import load_and_prep_image, predict_json, get_classes_and_models
+from utils import load_and_prep_image, predict, get_classes_and_models, update_logger
 import SessionState
  
-st.title("My first App in Streamlit.")
+st.title("Streamlit Web App for Image classification")
 st.header("Identify what's in your food photo!")
 
 @st.cache
-def makePrediction(image, model, class_name):		
-	image = load_and_prep_image(image)
+def make_prediction(image, model, class_name):		
+	image = load_and_prep_image(image, 224)
 	image = tf.cast(tf.expand_dims(image, axis=0), tf.int16)
-	preds = predict_json()
+	preds = predict(image)
 
 	pred_class = class_name[tf.argmax(preds[0])]
 	pred_conf = tf.reduce_max(preds[0])
@@ -21,13 +21,16 @@ def makePrediction(image, model, class_name):
 choose_model = st.sidebar.selectbox(
 	"Pick model to use",
 	# model name writing style - 'Model_number :' - after this can add discription. 
-	("Model_1 : ",
-	 "Model_2 : ",
-	 "Model_3 : ")
+	("Model_1 : EfficientNet - 10 classes",
+	 "Model_2 : Not available",
+	 "Model_3 : Not available")
 )
 
 # Model choice logic
-CLASSES, MODEL = get_classes_and_models(choose_model)
+try:
+	CLASSES, MODEL = get_classes_and_models(choose_model)
+except KeyError:
+	pass
 
 # Display info about model and classes
 if st.checkbox("Show classes"):
@@ -54,17 +57,18 @@ if pred_button:
 
 # If pressed
 if session_state.pred_button:
-	session_state.image, session_state.pred_class, session_state.pred_conf = makePrediction(session_state.uploaded_image, model=MODEL, class_name=CLASSES)
+	session_state.image, session_state.pred_class, session_state.pred_conf = make_prediction(session_state.uploaded_image, model=MODEL, class_name=CLASSES)
 	st.write("Prediction : {},\nConfidance : {}".format(session_state.pred_class, session_state.pred_conf))
 
 	# Feedback loop
-	session_state.feedback = st.selectbox("Correct?", ("Yes", "No"))
+	session_state.feedback = st.selectbox("Correct?", ("Select an option", "Yes", "No"))
 
 	if session_state.feedback == "Yes":
 		st.write("Thanks you for feedback!")
-		# write a mothod to update log
+		#TODO - code written to simply print log. update using logger.
+		update_logger(img=session_state.image, model_used=MODEL, pred_class=session_state.pred_class, pred_conf=session_state.pred_conf, correct=True)
 	elif session_state.feedback == "No":
 		session_state.correct_class = st.text_input("Select correct label for it.")
 		if session_state.correct_class:
 			st.write("Thank you for that, we'll use your help to make our model better!")
-			# write method to update log, with feedback label.
+			update_logger(img=session_state.image, model_used=MODEL, pred_class=session_state.pred_class, pred_conf=session_state.pred_conf, correct=False, user_label=session_state.correct_class)
--- utils.py
+++ utils.py
@@ -1,11 +1,16 @@
 import tensorflow as tf
 
 # dummy base prediction classes
-base_classes = ['fried_rice',
-					'hamburger',
-					'ice_cream',
-					'pizza',
-					'ramen']
+base_classes = ['chicken_curry',
+				 'chicken_wings',
+				 'fried_rice',
+				 'grilled_salmon',
+				 'hamburger',
+				 'ice_cream',
+				 'pizza',
+				 'ramen',
+				 'steak',
+				 'sushi']
 
 # updated classes will add here
 updated_classes = []
@@ -30,13 +35,26 @@ def load_and_prep_image(image, img_shape, rescale=False):
 	else:
 		return img
 
-def predict_json():
-	pass
+def predict(image):
+	trained_model = tf.keras.models.load_model('efficientnet_model_1_10_classes')
+	prediction = trained_model.predict(image)
+	return prediction
 
 def get_classes_and_models(model_name):
-	model_name = model_name.split(" :")[0].lower()
+	model_name = model_name.split(" : ")[0].lower()
 
 	CLASSSES = models[model_name]["classes"]
 	MODEL = models[model_name]["model_name"]
 
 	return CLASSSES, MODEL
+
+def update_logger(img, model_used, pred_class, pred_conf, correct=False, user_label=None):
+	logger = {
+		"image": img,
+		"model_used": model_used,
+		"pred_class": pred_class,
+		"pred_conf": pred_conf,
+		"correct": correct,
+		"user_label": user_label
+	}   
+	#return logger
